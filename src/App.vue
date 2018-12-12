ce8e5d37c6dadd8530129fee65d73ff2f0b6dd15
Mateus <mat3usrc@gmail.com>
2018-12-12 00:40:13 -0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <HelloWorld/>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      //
+    };
+  }
+};
+</script>
+
+<style>
+* {
+  font-family: "Ubuntu", sans-serif;
+}
+</style>
+
