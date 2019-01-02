58ea442b8b73656e5d3579de01ee2667b55f65a2
Mateus Rodrigues <mrodrigues@soujmv.com>
2019-01-02 11:18:21 -0200

    componentes novos

---
+++ src/components/Login.vue
@@ -0,0 +1,94 @@
+<template>
+  <v-container>
+    <v-layout justify-center mt-5>
+      <v-flex sm8 xs12>
+        <v-expansion-panel>
+          <v-expansion-panel-content expand-icon>
+            <div slot="header" class="panel-header title">Nova Conta</div>
+            <div class="panel-body">
+              <v-layout align-center justify-space-around row wrap my-4>
+                <h3 class="ma-2">Logar com redes sociais ou preencha o login</h3>
+                <div class="ma-2">
+                  <v-btn>Face</v-btn>
+                  <v-btn>Insta</v-btn>
+                  <v-btn>G+</v-btn>
+                </div>
+              </v-layout>
+              <v-form>
+                <v-layout row wrap>
+                  <v-text-field solo class="mr-4 email-input" placeholder="Email"></v-text-field>
+                  <v-text-field solo placeholder="Senha"></v-text-field>
+                </v-layout>
+                <v-btn block depressed class="btn-entrar">Entrar</v-btn>
+                <v-btn block flat>Esqueci minha senha</v-btn>
+              </v-form>
+            </div>
+          </v-expansion-panel-content>
+          <v-expansion-panel-content expand-icon>
+            <div slot="header" class="panel-header title">Registrar</div>
+            <div class="panel-body">
+              <v-layout align-center justify-space-around row wrap my-4>
+                <h3 class="ma-2">Logar com redes sociais ou preencha o login</h3>
+                <div class="ma-2">
+                  <v-btn>Face</v-btn>
+                  <v-btn>Insta</v-btn>
+                  <v-btn>G+</v-btn>
+                </div>
+              </v-layout>
+              <v-form>
+                <v-text-field solo class='new-acc-input' placeholder="Nome"></v-text-field>
+                <v-layout row wrap>
+                  <v-text-field solo class='new-acc-input' placeholder="Campo teste"></v-text-field>
+                  <v-text-field solo class='new-acc-input' placeholder="Campo teste"></v-text-field>
+                  <v-text-field solo class='new-acc-input' placeholder="Campo teste"></v-text-field>
+                  <v-text-field solo class='new-acc-input' placeholder="Campo teste"></v-text-field>
+                  <v-text-field solo class='new-acc-input' placeholder="Campo teste"></v-text-field>
+                  <v-text-field solo class='new-acc-input' placeholder="Campo teste"></v-text-field>
+                  <v-text-field solo class='new-acc-input' placeholder="Campo teste"></v-text-field>
+                </v-layout>
+                <v-btn block depressed class="btn-entrar">Cadastre-se</v-btn>
+              </v-form>
+            </div>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({})
+};
+</script>
+
+<style scoped>
+.panel-header {
+  margin: 1em 3em;
+}
+.panel-body {
+  padding: 3em 6em;
+  border-top: 5px solid grey;
+}
+.btn-entrar {
+  padding: 2.5em 0;
+}
+.v-expansion-panel__header {
+  border-bottom: 5px solid black;
+}
+.new-acc-input {
+  margin-right: 1.5em;
+}
+
+@media (max-width: 1264px) {
+  .panel-body {
+    padding: 0 1em;
+  }
+  .email-input {
+    margin-right: 0 !important;
+  }
+  .panel-header {
+    text-align: center;
+  }
+}
+</style>
---
+++ src/components/Payment.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container>
+    <v-layout justify-center>
+      <v-flex sm8 xs12>
+        <v-card>
+          <v-card-title class="card-title px-5">Pagamento</v-card-title>
+          <v-card-text>
+            <v-layout row wrap justify-space-around>
+              <v-flex xs4>
+                <h4 class="mb-4">Cupom de desconto</h4>
+                <v-text-field solo placeholder="numero"></v-text-field>
+              </v-flex>
+              <v-flex xs4>
+                <h4 class="mb-4">Forma de pagamento</h4>
+                <v-radio-group>
+                  <v-radio class='ma-2' label="teste"></v-radio>
+                  <v-radio class='ma-2' label="teste"></v-radio>
+                  <v-radio class='ma-2' label="teste"></v-radio>
+                  <v-radio class='ma-2' label="teste"></v-radio>
+                </v-radio-group>
+              </v-flex>
+            </v-layout>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.card-title {
+  border-bottom: 3px solid grey;
+}
+</style>
